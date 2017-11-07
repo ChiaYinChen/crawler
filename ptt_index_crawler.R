d70130e766c48cf8b1dba045acaf45e69cce98b2
chiayinchen <awdrg1210@gmail.com>
2017-11-07 09:21:38 +0800

    add ptt board last multiple page crawler

--- ptt_index_crawler.R
+++ ptt_index_crawler.R
@@ -117,3 +117,49 @@ result$page5200$articles[[20]]
 #   articles[[i]] <- article_detail_crawler(result$page5200$links[i])
 # }
 # articles
+
+
+
+single_page_crawler <- function(page_url) {
+  n_recs_css <- ".hl"
+  article_title_css <- ".title a"
+  article_date_css <- ".date"
+  author_css <- ".author"
+  
+  html_doc <- read_html(page_url)
+  page_content <- list()
+  cols <- c(n_recs_css, article_title_css, article_date_css, author_css)
+  for (i in 1:length(cols)) {
+    page_content[[i]] <- html_doc %>%
+      html_nodes(css = cols[i]) %>%
+      html_text
+  }
+  return(page_content)
+}
+
+multi_page_crawler <- function(page_url, n_pages) {
+  # get last_page_number
+  last_page <- ".wide:nth-child(2)"
+  last_page_url <- read_html(url) %>%
+    html_nodes(css = last_page) %>%
+    html_attr("href")
+  
+  number_pattern <- "[0-9]+"
+  regexpr_m <- gregexpr(pattern = number_pattern, last_page_url)
+  last_page_number <- last_page_url %>%
+    regmatches(m = regexpr_m) %>%
+    as.numeric
+  
+  page_numbers <- c((last_page_number - n_pages + 2):last_page_number, "")
+  page_urls <- paste0("https://www.ptt.cc/bbs/NBA/index", page_numbers, ".html")
+  
+  pages_list <- list()
+  for (i in 1:length(page_urls)) {
+    pages_list[[i]] <- single_page_crawler(page_urls[i])
+  }
+  return(pages_list)
+}
+
+# 呼叫函數
+url <- "https://www.ptt.cc/bbs/NBA/index.html"
+nba_5_pages <- multi_page_crawler(url, n_pages = 5)
